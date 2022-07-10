3b0a14b4c25e092a8ddffe3f88563e22183c9dd7
pengyuan shu <ssyps2@nottingham.edu.cn>
2022-07-10 19:10:18 +0800

    transformation for oneleg has almost completed

--- src/model_and_simulation/robot.m
+++ src/model_and_simulation/robot.m
@@ -7,12 +7,12 @@ theta=[0.5,0.8,1.1];     % should be the feedback angle by servo
 %             a(i-1)  alpha(i-1)  offset(i)  theta(i)
 Modified_DH = [0           0	    0   	 theta(1,1);
                0.043	 -pi/2      0        theta(1,2);
-               0.073	   0   	    0   	 theta(1,3)];   % length of link3 is 0.133m
+               0.073	   pi  	    0   	 theta(1,3)];   % length of link3 is 0.133m
 
 %             a(i)   alpha(i)  offset(i)  theta(i)
 Standard_DH = [0.043   -pi/2	   0   	  theta(1,1);
-               0.073	0          0      theta(1,2);
-               0.133	0   	   0   	  theta(1,3)];
+               0.073	 pi        0      theta(1,2);
+               0.133	 0   	   0   	  theta(1,3)];
 
 hexa = rigidBodyTree;
 
--- src/model_and_simulation/transformation.m
+++ src/model_and_simulation/transformation.m
@@ -2,63 +2,68 @@ clear;
 clc;
 
 cmd_spd = [0.001,0,0];   % x, y, z cmd speed
+angle_base2leg = [atan2(6,12),pi/2,pi-atan2(6,12),-atan2(6,12),-pi/2,atan2(6,12)-pi];  % related to z-axis, leg1~6
+length_base2leg = 0.18;  % unit:m
+
 theta_zero = [0,0,0];
-theta_stand = [0,deg2rad(12),deg2rad(28.8)];   % should be the feedback angle by servo in rad
-theta_front = [deg2rad(57.6),deg2rad(21.6),deg2rad(55.2)];
-theta_up = [deg2rad(24),deg2rad(60),deg2rad(72)];
+theta_stand = [0,deg2rad(-12),deg2rad(-74)];   % should be the feedback angle by servo in rad
+theta_front = [deg2rad(-41),deg2rad(-22),deg2rad(-100)];
+theta_back = [deg2rad(7.2),deg2rad(2.4),deg2rad(-43)];
+theta_up = [deg2rad(-24),deg2rad(-60),deg2rad(-117)];
+
+theta = theta_stand;
 
 %              theta(i)  offset(i)  a(i-1)  alpha(i-1)
-Modified_DH = [theta_stand(1,1)   0        0         0;
-               theta_stand(1,2)   0        0.043   -pi/2;
-               theta_stand(1,3)   0        0.073     0];   % length of link3 is 0.133m
+modified_DH = [theta(1,1)   0        0         0;
+               theta(1,2)   0        0.043   -pi/2;
+               theta(1,3)   0        0.073     pi];   % length of link3 is 0.133m
 
 %             theta(i)   offset(i)  a(i)  alpha(i)
-Standard_DH = [theta_stand(1,1)   0      0.043   -pi/2;
-               theta_stand(1,2)   0      0.073     0;
-               theta_stand(1,3)   0      0.133     0];
-
-L1 = Link(Standard_DH(1,:),'standard');  % length in m, revolute joint
-L2 = Link(Standard_DH(2,:),'standard');
-L3 = Link(Standard_DH(3,:),'standard');
-
-leg4 = SerialLink([L1,L2,L3],'name','leg1');
+standard_DH = [theta(1,1)   0      0.043   -pi/2;
+               theta(1,2)   0      0.073     pi;
+               theta(1,3)   0      0.133     0];
 
 %% matrix transformation for one leg
 %  rotx/y/z would related to fixed angle, so that:
 %  euler angle: z->y->x => fixed angle: x->y->z, they're equivalent
-R0_1 = rotz(theta_stand(1,1)) * rotx(-90,'deg');    % fixed angle: x->z
-R1_2 = rotz(theta_stand(1,2));
-R2_3 = rotz(theta_stand(1,3));
+%  for the leg, in euler angle: z1->x1->z2->z3
+R0_1 = [rotz(theta(1,1)),[0;0;0]; 0,0,0,1];
+R1_2 = [rotx(standard_DH(1,4))*rotz(theta(1,2)),[0;0;0]; 0,0,0,1];
+R2_3 = [rotx(standard_DH(2,4))*rotz(theta(1,3)),[0;0;0]; 0,0,0,1];
 
-D0_1 = [0.043; 0; 0];
-D1_2 = [0.073; 0; 0];
-D2_3 = [0.133; 0; 0];
+T0_1 = [eye(3,3),[0.043; 0; 0]; 0,0,0,1];
+T1_2 = [eye(3,3),[0.073; 0; 0]; 0,0,0,1];
+T2_3 = [eye(3,3),[0.133; 0; 0]; 0,0,0,1];
 
-H0_1 = [R0_1,D0_1; 0,0,0,1];
-H1_2 = [R1_2,D1_2; 0,0,0,1];
-H2_3 = [R2_3,D2_3; 0,0,0,1];
+HTM_leg = R0_1 * T0_1 * R1_2 * T1_2 * R2_3 * T2_3;
+HTM_base2leg = cell(6,1);
 
-% HTM_base_leg1 = 
+for i = 1:6
+    HTM_base2leg{i} = [rotz(angle_base2leg(1,i)),[0;0;0]; 0,0,0,1] * transl([length_base2leg;0;0]) * HTM_leg;
+end
 
-HTM_shld = H0_1 * H1_2 * H2_3;   % foot relative to the shoulder coordinate
+%% robot kinematics model
+L1 = Link(standard_DH(1,:),'standard');  % length in m, revolute joint
+L2 = Link(standard_DH(2,:),'standard');
+L3 = Link(standard_DH(3,:),'standard');
 
-HTM = leg4.fkine(theta_stand);
-leg_angle = leg4.ikine(HTM,'mask',[1,1,1,0,0,0]);
+% robot_leg = cell(6,1);
+robot_leg = SerialLink([L1,L2,L3],'name','leg3');
 
-start_pos = HTM;
-% pace = 0.1;    % unit:cm
-% HTM.t(1,1) = HTM.t(1,1) + pace;
-end_pos = leg4.fkine(theta_front);
-q1 = leg4.ikine(start_pos,'mask',[1,1,1,0,0,0]);
-q2 = leg4.ikine(end_pos,'mask',[1,1,1,0,0,0]);
+start_pos = robot_leg.fkine(theta);
+% pace = 0.1;    % unit:m
+% start_pos.t(1,1) = start_pos.t(1,1) + pace;
+end_pos = robot_leg.fkine(theta_front);
+q1 = robot_leg.ikine(start_pos,'mask',[1,1,1,0,0,0]);
+q2 = robot_leg.ikine(end_pos,'mask',[1,1,1,0,0,0]);
 
-sample_dot = 10;
-[q ,qd, qdd] = jtraj(q1,q2,sample_dot); % 五次多项式轨迹，得到关节角度，角速度，角加速度，10为采样点个数
+sample_dot = 20;
+[q ,qd, qdd] = jtraj(q1,q2,sample_dot); % 五次多项式轨迹，得到关节角度，角速度，角加速度，20为采样点个数
 
 grid on
-end_trial = leg4.fkine(q);%根据插值，得到末端执行器位姿
+end_trial = robot_leg.fkine(q);%根据插值，得到末端执行器位姿
 % plot3(end_trial(:).t(1,1),end_trial(:).t(1,2),end_trial(:).t(1,3));%输出末端轨迹
 hold on
 
-leg4.plot(q);
-% leg1.teach();
+robot_leg.plot(q);
+% robot_leg.teach();
