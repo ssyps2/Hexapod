6130445304282fdb0e2ea331393630fbf4df4000
pengyuan shu <ssyps2@nottingham.edu.cn>
2022-07-11 17:48:08 +0800

    update on 11-July 17:48

---
+++ src/model_and_simulation/cmdHexapodMove.m
@@ -0,0 +1,75 @@
+%% robotics toolbox (by Peter Corke) has been used in this script
+function [pulse] = cmdHexapodMove(cmd_vel)
+    cmd_vel = [0.001,0,0];   % x, y, z cmd velocity
+    pulse = cell(6,3);
+
+    angle_base2leg = [atan2(6,12),pi/2,pi-atan2(6,12),-atan2(6,12),-pi/2,atan2(6,12)-pi];  % related to z-axis, leg1~6
+    length_base2leg = 0.18;  % unit:m
+    
+    theta_zero = [0,0,0];
+    theta_stand = [0,deg2rad(-12),deg2rad(-74)];   % should be the feedback angle by servo in rad
+    theta_front = [deg2rad(-41),deg2rad(-22),deg2rad(-100)];
+    theta_back = [deg2rad(7.2),deg2rad(2.4),deg2rad(-43)];
+    theta_up = [deg2rad(-24),deg2rad(-60),deg2rad(-117)];
+    
+    theta = theta_stand;
+    
+    %              theta(i)  offset(i)  a(i-1)  alpha(i-1)
+    modified_DH = [theta(1,1)   0        0         0;
+                   theta(1,2)   0        0.043   -pi/2;
+                   theta(1,3)   0        0.073     pi];   % length of link3 is 0.133m
+    
+    %             theta(i)   offset(i)  a(i)  alpha(i)
+    standard_DH = [theta(1,1)   0      0.043   -pi/2;
+                   theta(1,2)   0      0.073     pi;
+                   theta(1,3)   0      0.133     0];
+    
+    %% matrix transformation for one leg
+    %  rotx/y/z would related to fixed angle, so that:
+    %  euler angle: z->y->x => fixed angle: x->y->z, they're equivalent
+    %  for the leg, in euler angle: z1->x1->z2->z3
+    R0_1 = [rotz(theta(1,1)),[0;0;0]; 0,0,0,1];
+    R1_2 = [rotx(standard_DH(1,4))*rotz(theta(1,2)),[0;0;0]; 0,0,0,1];
+    R2_3 = [rotx(standard_DH(2,4))*rotz(theta(1,3)),[0;0;0]; 0,0,0,1];
+    R0_3 = R0_1 * R1_2 * R2_3;
+    R3_0 = transpose(R0_3);
+    
+    T0_1 = [eye(3,3),[0.043; 0; 0]; 0,0,0,1];
+    T1_2 = [eye(3,3),[0.073; 0; 0]; 0,0,0,1];
+    T2_3 = [eye(3,3),[0.133; 0; 0]; 0,0,0,1];
+    
+    HTM_leg = R0_1 * T0_1 * R1_2 * T1_2 * R2_3 * T2_3;
+    HTM_base2leg = cell(6,1);
+    
+    for i = 1:6
+        HTM_base2leg{i} = [rotz(angle_base2leg(1,i)),[0;0;0]; 0,0,0,1] * transl(length_base2leg,0,0) * HTM_leg;
+    end
+    
+    %% robot kinematics model
+%     legs = 
+    L1 = Link(standard_DH(1,:),'standard');  % length in m, revolute joint
+    L2 = Link(standard_DH(2,:),'standard');
+    L3 = Link(standard_DH(3,:),'standard');
+    
+    % robot_leg = cell(6,1);
+    robot_leg = SerialLink([L1,L2,L3],'name','leg3');
+%     config = homeConfiguration(robot_leg)
+    
+    start_pos = robot_leg.fkine(theta);
+    % pace = 0.1;    % unit:m
+    % start_pos.t(1,1) = start_pos.t(1,1) + pace;
+    end_pos = robot_leg.fkine(theta_front);
+    q1 = robot_leg.ikine(start_pos,'mask',[1,1,1,0,0,0]);
+    q2 = robot_leg.ikine(end_pos,'mask',[1,1,1,0,0,0]);
+    
+    sample_dot = 20;
+    [q ,qd, qdd] = jtraj(q1,q2,sample_dot); % 五次多项式轨迹，得到关节角度，角速度，角加速度，20为采样点个数
+    
+    grid on
+    end_trial = robot_leg.fkine(q);%根据插值，得到末端执行器位姿
+    % plot3(end_trial(:).t(1,1),end_trial(:).t(1,2),end_trial(:).t(1,3));%输出末端轨迹
+    hold on
+    
+    robot_leg.plot(theta);
+    % robot_leg.teach();
+end
--- src/robot_control/src/kinematics.py
+++ src/robot_control/src/kinematics.py
@@ -1,18 +1,74 @@
 import time
+
+from sympy import rot_axis3
 import Board
 import numpy as np
 import spatialmath as sm
 import roboticstoolbox as rtb
 
-# leg joint servo ID
-leg1 = (7,8,9)
-leg2 = (4,5,6)
-leg3 = (1,2,3)
-leg4 = (10,11,12)
-leg5 = (13,14,15)
-leg6 = (16,17,18)
+# constants define
+TASK_FREQ = 500 # task frequency in Hz
+SERVO_ANGLE_RANGE = 240 # in degree
+SERVO_MAX_PULSE = 1000
+SERVO_HALF_PULSE = SERVO_MAX_PULSE/2  # 500
+PULSE2ANGLE = SERVO_ANGLE_RANGE/SERVO_MAX_PULSE  # 0.24, in degree
 
-# def cmdHexpodMove(x,y,z):
+## init joint servo ID in legs
+leg_ID_lambda = lambda id: (id,id+1,id+2)
+#               leg1            leg2             leg3              leg4             leg5              leg6
+legs_ID = (leg_ID_lambda(7),leg_ID_lambda(4),leg_ID_lambda(1),leg_ID_lambda(10),leg_ID_lambda(13),leg_ID_lambda(16))
 
+## feedback angle by servo in rad (for IK)
+def getJointAngle():
+    """
+    :retval: legs_joint_angle[legNum][jointID]
+    """
+    pulse2angle_lambda = lambda pulse: (pulse-SERVO_HALF_PULSE)*PULSE2ANGLE # in degree
+    leg_joint_angle_lambda = lambda i: [pulse2angle_lambda(Board.getBusServoPulse(legs_ID[i][0])),\
+                                        -pulse2angle_lambda(Board.getBusServoPulse(legs_ID[i][1])),\
+                                        pulse2angle_lambda(Board.getBusServoPulse(legs_ID[i][2]))]
+
+    # from leg1 to leg6, leg3~6 was inversed because of assembling
+    legs_joint_angle = [ leg_joint_angle_lambda(0), leg_joint_angle_lambda(1), leg_joint_angle_lambda(2),\
+                        -leg_joint_angle_lambda(3), -leg_joint_angle_lambda(4), -leg_joint_angle_lambda(5) ]
+
+    return legs_joint_angle
+
+## DH paramter for each leg: offset(i), a(i), alpha(i), theta(i)
+def updateStandardDH():
+    legs_joint_angle = getJointAngle()
+
+    standard_DH_lambda = lambda legNum: [ [0, 0.043, -np.pi/2, legs_joint_angle[legNum][0]],\
+        [0, 0.073, np.pi, legs_joint_angle[legNum][1]], [0, 0.133, 0, legs_joint_angle[legNum][2]] ]
+
+    hexapod_leg1 = rtb.DHRobot([rtb.RevoluteDH(standard_DH_lambda(0)[0]),\
+        rtb.RevoluteDH(standard_DH_lambda(0)[1]),rtb.RevoluteDH(standard_DH_lambda(0)[2])])
+
+## matrix transformation for one leg
+#  rotx/y/z would related to fixed angle, so that:
+#  euler angle: z->y->x => fixed angle: x->y->z, they're equivalent
+#  for the leg, in euler angle: z1->x1->z2->z3
+
+# R0_1 = [rtb.ET.tz(theta(1,1)),[0;0;0]; 0,0,0,1];
+# R1_2 = [rotx(standard_DH(1,4))*rotz(theta(1,2)),[0;0;0]; 0,0,0,1];
+# R2_3 = [rotx(standard_DH(2,4))*rotz(theta(1,3)),[0;0;0]; 0,0,0,1];
+# R0_3 = R0_1 * R1_2 * R2_3;
+# R3_0 = transpose(R0_3);
+
+# T0_1 = [eye(3,3),[0.043; 0; 0]; 0,0,0,1];
+# T1_2 = [eye(3,3),[0.073; 0; 0]; 0,0,0,1];
+# T2_3 = [eye(3,3),[0.133; 0; 0]; 0,0,0,1];
+
+# HTM_leg = R0_1 * T0_1 * R1_2 * T1_2 * R2_3 * T2_3;
+# HTM_base2leg = cell(6,1);
+
+# for i = 1:6
+#     HTM_base2leg{i} = [rotz(angle_base2leg(1,i)),[0;0;0]; 0,0,0,1] * transl(length_base2leg,0,0) * HTM_leg;
+
+## input x,y,z cmd velocity, integrate it and generate the trajectory
+#  according to its current position, and return the pulse values of 
+#  each joint servo
+def cmdHexpodMove(x,y,z):
+    
 
 # Board.setBusServoPulse(id, pulse, 1000)
