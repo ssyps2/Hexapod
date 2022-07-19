5d0dcc9582842747f0b84690c8fc5ab1fb01f3f8
pengyuan shu <ssyps2@nottingham.edu.cn>
2022-07-19 22:56:55 +0800

    changed src directory name to scripts for python3 file

---
+++ src/robot_control/scripts/vel_publisher.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+from geometry_msgs.msg import Twist
+
+CONTROL_FREQ = 30  # Hz
+
+def velocity_publisher():
+    rospy.init_node('velocity_publisher', anonymous=True)
+
+	# topic name: /hex/cmd_vel; message type: geometry_msgs::Twist
+    turtle_vel_pub = rospy.Publisher('/hex/cmd_vel', Twist, queue_size=10)
+
+    rate = rospy.Rate(CONTROL_FREQ)
+
+    while not rospy.is_shutdown():
+        vel_msg = Twist()
+        vel_msg.linear.x = 0.1  # max: 0.15
+        vel_msg.angular.z = 0.05
+
+        turtle_vel_pub.publish(vel_msg)
+        rospy.loginfo("x:%0.2fm/s, y:%0.2fm/s, z:%0.2frad/s]", vel_msg.linear.x, vel_msg.linear.y, vel_msg.angular.z)
+
+        rate.sleep() # delay in a rate of given Hz
+
+if __name__ == '__main__':
+    try:
+        velocity_publisher()
+    except rospy.ROSInterruptException:
+        pass
+
+
